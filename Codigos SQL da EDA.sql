f601f5e73b056556e6ed7fc3218d1555e30c4cf1
Leonardo Bonani <126993350+Leonardo-Bonani@users.noreply.github.com>
2023-05-09 11:12:22 -0300

    Update Codigos SQL da EDA.sql

--- Codigos SQL da EDA.sql
+++ Codigos SQL da EDA.sql
@@ -1,17 +1,20 @@
--- Primeiramente, foi consultado o número total de avaliações, contando o número de linhas da tabela REVIEWS
+-- Número total de avaliações, contando (COUNT) o número de linhas da tabela REVIEWS
 
 select COUNT(RATING)
 FROM REVIEWS
 
 -- A consulta foi adicionada ao Dashboard (Qtd de Avaliações: 1,112)
 
--- A segunda consulta 
-select avg(RATING) from REVIEWS
+-- Média de avaliações,  média (avg) de avaliações da tabela REVIEWS
 
+select avg(RATING) 
+from REVIEWS
 
+-- A consulta foi adicionada ao Dashboard como gráfico de Manômetro 
 
+-- Quantidade de produtos avaliados por categoria, contando (count) os valosres distintos (distinct) de IDs de produtos (PRODUCT_ID) da tabela REVIEWS
+-- Necessario um leftjoin para trazer as informações de categorias da tabela PRODUCTS
 
--- (inserir texto)
 select 
        CATEGORY,
        count(Distinct(PRODUCT_ID)) as "QUANTIDADE DE PRODUTOS AVALIADOS"
@@ -22,10 +25,11 @@ left join PRODUCTS on REVIEWS.PRODUCT_ID = PRODUCTS.ID
 group by CATEGORY
 order by CATEGORY
 
+-- A consulta foi adicionada ao Dashboard em um gráfico de barras horizontais 
 
+-- Média de avaliaçõess por categoria, média (avg) de avaliações (RATING) da tabela REVIEWS
+-- Necessario um leftjoin para trazer as informações de categorias da tabela PRODUCTS
 
-
--- (inserir texto)
 select 
      CATEGORY,
      AVG(REVIEWS.RATING)
@@ -37,9 +41,10 @@ GROUP BY (CATEGORY)
 ORDER BY CATEGORY
 
 
+-- TOP 10 prodtuos em avaliações, média (avg) de avaliações (RATING) da tabela REVIEWS
+-- Necessario um leftjoin para trazer as informações de categorias da tabela PRODUCTS
+-- Agrupados pelo título, Ordenados pela média e limitados a 10 linhas. 
 
-
--- (inserir texto)
 select 
       TITLE,
       avg(REVIEWS.RATING) as Media_de_Avaliações
@@ -51,10 +56,10 @@ group by ( TITLE)
 order by Media_de_Avaliações desc
 limit 10
 
+-- Tabela Média de Quantidade Vendida por Nota (Avaliações) de Produtos , contagem (count) de vendas da tabela ORDERS, média (avg) de avaliações (RATING) da tabela PRODUCTS
+-- Necessario um leftjoin para trazer as informações de categorias da tabela PRODUCTS
+-- Agrupados pelo IDs, Ordenados pela contagem (count) e limitados a 15 linhas. 
 
-
-
--- (inserir texto)
 select 
       
       PRODUCTS.ID,
